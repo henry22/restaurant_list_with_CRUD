d52853641f758501fbfcaa0d67ea59653bd64a39
henry22 <henry811010@gmail.com>
2019-11-17 15:03:35 +0800

    Create new restaurant page and install body-parser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const expressHandlebars = require('express-handlebars')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 // Use mongoose to connect to the mongodb server
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,6 +17,7 @@ app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const Restaurant = require('./models/restaurant')
 
@@ -27,6 +29,17 @@ app.get('/', (req, res) => {
   })
 })
 
+// Read all restaurants
+app.get('/restaurants', (req, res) => {
+  return res.redirect('/')
+})
+
+// Create one restaurant page
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+// Read one restaurant
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurantId = req.params.restaurant_id
 
@@ -36,6 +49,12 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
     return res.render('show', { restaurant: restaurant })
   })
 })
+
+// Create one restaurant
+app.post('/restaurants', (req, res) => {
+
+})
+
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
