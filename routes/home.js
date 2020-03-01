74d0f956d2464b4d6715d3eadc8ffb5ff659dabb
henry22 <henry811010@gmail.com>
2020-03-02 03:16:34 +0800

    Add sorting function

--- routes/home.js
+++ routes/home.js
@@ -4,12 +4,33 @@ const router = express.Router()
 const Restaurant = require('../models/restaurant')
 
 router.get('/', (req, res) => {
+  let dropdownDisplay = '排序'
+  let sortingPattern = {}
+  switch (req.query.sort) {
+    case 'asc':
+      sortingPattern = { name: 'asc' }
+      dropdownDisplay = 'A -> Z'
+      break;
+    case 'desc':
+      sortingPattern = { name: 'desc' }
+      dropdownDisplay = 'Z -> A'
+      break;
+    case 'category':
+      sortingPattern = { category: 'asc' }
+      dropdownDisplay = '類別'
+      break;
+    case 'location':
+      sortingPattern = { location: 'asc' }
+      dropdownDisplay = '地區'
+      break;
+  }
   Restaurant.find()
+    .sort(sortingPattern)
     .lean()
     .exec((err, restaurants) => {
       if (err) return console.log(err)
 
-      return res.render('index', { restaurants: restaurants })
+      return res.render('index', { restaurants: restaurants, dropdownDisplay: dropdownDisplay})
     })
 })
 
