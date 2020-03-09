5a1d244baab325f96f5cad22d0c7107308298025
henry22 <henry811010@gmail.com>
2020-03-09 23:10:53 +0800

    POST register

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 // routes/user.js
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -15,7 +16,30 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, password2 } = req.body
+
+  User.findOne({ email: email }).then(user => {
+    if (user) {
+      console.log('User already exists')
+
+      res.render('register', {
+        name,
+        email,
+        password,
+        password2
+      })
+    } else {
+      const newUser = new User({
+        name,
+        email,
+        password
+      })
+
+      newUser.save().then(user => {
+        res.redirect('/')
+      }).catch(err => console.error(err))
+    }
+  })
 })
 
 router.get('/logout', (req, res) => {
