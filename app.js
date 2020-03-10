842d8c3d71f0eacdeeda6117bbe6395303227525
henry22 <henry811010@gmail.com>
2020-03-10 11:12:23 +0800

    Add locals for adding variables into views and add login logout icons

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  res.locals.isAuthenticated = req.isAuthenticated()
+  next()
+})
+
 // import passport config
 require('./config/passport')(passport)
 
