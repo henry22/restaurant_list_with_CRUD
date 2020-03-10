60ab70d5110684455be2273b28d79a450f20ad7a
henry22 <henry811010@gmail.com>
2020-03-10 11:48:11 +0800

    Install bcryptjs for hashing the password

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -39,9 +40,17 @@ router.post('/register', (req, res) => {
         password
       })
 
-      newUser.save().then(user => {
-        res.redirect('/')
-      }).catch(err => console.error(err))
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if(err) throw err
+
+          newUser.password = hash
+
+          newUser.save().then(user => {
+            res.redirect('/')
+          }).catch(err => console.error(err))
+        })
+      })
     }
   })
 })
